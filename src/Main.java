b222d31a36bfb755ad9eaa37c99d63429db70e2d
Nhat Lam Nguyen <135177207+nhlam3011@users.noreply.github.com>
2024-03-14 08:33:54 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-
-
-        Bài làm viết tại đây
-
+        String input = "input.txt";
+        String output = "output.txt";
+        thaytheFile(input, output);
+    }
+    private static void thaytheFile(String input, String output) {
+        try {
+            FileInputStream fis = new FileInputStream(input);
+            Scanner scanner = new Scanner(fis);
+            FileWriter writer = new FileWriter(output);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                line = line.replaceAll("Nha Trang", "Vũng Tàu");
+                writer.write(line + "\n");
+            }
+            scanner.close();
+            writer.close();
+            fis.close();
+            System.out.println("Thay thế thành công.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
